3f3a55483a55beee01dd98bcf2b668569cfae2fa
Paul Black <pqblack@pm.me>
2024-06-22 14:33:40 -0400

    Set up project with webpack and gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
-
   return {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
-      publicPath: '/'
+      publicPath: isProduction ? '/Savior/' : '/'
     },
     module: {
       rules: [
@@ -44,6 +44,11 @@ module.exports = (env, argv) => {
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: './public/index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'public/404.html', to: '404.html' },
+        ],
       }),
       isProduction && new MiniCssExtractPlugin({
         filename: '[name].[contenthash].css'
